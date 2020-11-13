58501765104224215a779298d170b7d9d72087fe
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2020-11-12 19:50:39 -0600

    [fixing] trying to make packages not push

---
+++ nodeModule.js
@@ -0,0 +1,12 @@
+module.exports = {
+    favFoods,
+}
+
+let foods = ['salad', 'pizza', 'burgers', 'fajtias' ]
+
+function favFoods() {
+    for (i = 0; i < foods.length; i++) {
+        foods[i]
+        return foods;
+    }
+}
